9702fc9cfdbf32d4e3cea19c37be0035376b9fca
chemch <cac692@gmail.com>
2023-08-11 21:13:56 -0500

    cc

---
+++ src/main/java/com/luchavor/commandrunner/command/DeleteFindingsCommand.java
@@ -0,0 +1,48 @@
+package com.luchavor.commandrunner.command;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Configuration;
+
+import com.luchavor.neo4japi.persistence.detection.DetectionRepo;
+import com.luchavor.neo4japi.persistence.detection.ZeekNotificationRepo;
+import com.luchavor.neo4japi.persistence.detection.state.ClosedDetectionStateRepo;
+import com.luchavor.neo4japi.persistence.detection.state.OpenDetectionStateRepo;
+import com.luchavor.neo4japi.persistence.finding.FindingRepo;
+import com.luchavor.neo4japi.persistence.finding.state.ConfirmedFindingStateRepo;
+import com.luchavor.neo4japi.persistence.finding.state.PotentialFindingStateRepo;
+import com.luchavor.neo4japi.persistence.finding.state.RefutedFindingStateRepo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+@ConditionalOnExpression("${command.deleteFindings.enabled:true}")
+public class DeleteFindingsCommand implements CommandLineRunner {
+	
+	@Autowired
+	RefutedFindingStateRepo refutedFindingStateRepo;
+	
+	@Autowired
+	PotentialFindingStateRepo potentialFindingStateRepo;
+	
+	@Autowired
+	ConfirmedFindingStateRepo confirmedFindingStateRepo;
+	
+	@Autowired
+	FindingRepo findingRepo;
+	
+	@Autowired
+	ZeekNotificationRepo zeekNotificationRepo;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// delete all objects
+		findingRepo.deleteAll();
+		confirmedFindingStateRepo.deleteAll();
+		potentialFindingStateRepo.deleteAll();
+		refutedFindingStateRepo.deleteAll();
+		log.info("Delete Findings command complete.");
+	}
+}
