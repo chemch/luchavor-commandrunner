44c85dccb4f17428cd3ba2cff220b7f8c73a2471
chemch <cac692@gmail.com>
2023-08-29 21:45:39 -0500

    xx

--- src/main/java/com/luchavor/commandrunner/command/DeleteArtifactsCommand.java
+++ src/main/java/com/luchavor/commandrunner/command/DeleteArtifactsCommand.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Configuration;
-
 import com.luchavor.neo4japi.persistence.artifact.ArtifactRepo;
 import com.luchavor.neo4japi.persistence.artifact.network.session.ConnectionRepo;
 import com.luchavor.neo4japi.persistence.artifact.network.session.DnsEventRepo;
@@ -12,25 +11,12 @@ import com.luchavor.neo4japi.persistence.artifact.network.session.HttpEventRepo;
 import com.luchavor.neo4japi.persistence.artifact.network.session.SessionRepo;
 import com.luchavor.neo4japi.persistence.artifact.network.session.state.ClosedSessionStateRepo;
 import com.luchavor.neo4japi.persistence.artifact.network.session.state.OpenSessionStateRepo;
-import com.luchavor.neo4japi.persistence.artifact.state.CompleteArtifactStateRepo;
-import com.luchavor.neo4japi.persistence.artifact.state.EmptyArtifactStateRepo;
-import com.luchavor.neo4japi.persistence.artifact.state.PartialArtifactStateRepo;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @Slf4j
 @ConditionalOnExpression("${command.deleteArtifacts.enabled:true}")
 public class DeleteArtifactsCommand implements CommandLineRunner {
-	
-	@Autowired
-	CompleteArtifactStateRepo completeArtifactStateRepo;
-	
-	@Autowired
-	PartialArtifactStateRepo partialArtifactStateRepo;
-	
-	@Autowired
-	EmptyArtifactStateRepo emptyArtifactStateRepo;
 	
 	@Autowired
 	ArtifactRepo artifactRepo;
@@ -55,10 +41,6 @@ public class DeleteArtifactsCommand implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		// delete artifact state objects
-		completeArtifactStateRepo.deleteAll();
-		partialArtifactStateRepo.deleteAll();
-		emptyArtifactStateRepo.deleteAll();
 		artifactRepo.deleteAll();
 		// delete sessions
 		openSessionStateRepo.deleteAll();
