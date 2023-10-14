5cb627474c9016918e28986d8d10ea371acbc680
chemch <cac692@gmail.com>
2023-10-14 14:49:14 -0400

    uu

--- src/main/java/com/luchavor/commandrunner/command/DeleteArtifactsCommand.java
+++ src/main/java/com/luchavor/commandrunner/command/DeleteArtifactsCommand.java
@@ -9,8 +9,6 @@ import com.luchavor.neo4japi.persistence.artifact.network.session.ConnectionRepo
 import com.luchavor.neo4japi.persistence.artifact.network.session.DnsEventRepo;
 import com.luchavor.neo4japi.persistence.artifact.network.session.HttpEventRepo;
 import com.luchavor.neo4japi.persistence.artifact.network.session.SessionRepo;
-import com.luchavor.neo4japi.persistence.artifact.network.session.state.ClosedSessionStateRepo;
-import com.luchavor.neo4japi.persistence.artifact.network.session.state.OpenSessionStateRepo;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
@@ -25,12 +23,6 @@ public class DeleteArtifactsCommand implements CommandLineRunner {
 	SessionRepo sessionRepo;
 	
 	@Autowired
-	ClosedSessionStateRepo closedSessionStateRepo;
-	
-	@Autowired
-	OpenSessionStateRepo openSessionStateRepo;
-	
-	@Autowired
 	DnsEventRepo dnsEventRepo;
 	
 	@Autowired
@@ -42,9 +34,6 @@ public class DeleteArtifactsCommand implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		artifactRepo.deleteAll();
-		// delete sessions
-		openSessionStateRepo.deleteAll();
-		closedSessionStateRepo.deleteAll();
 		sessionRepo.deleteAll();
 		// delete all events and encompassing objects
 		connectionRepo.deleteAll();
